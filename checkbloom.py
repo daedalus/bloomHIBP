3028e7285094202c47937f14a9a7f482338d2862
Dario Clavijo <daedalus2027@gmail.com>
2018-06-15 16:43:46 -0300

    Initial commit

---
+++ checkbloom.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# Author Dario Clavijo 2017
+# GPlv3
+
+# used for checking have i been pwnd passwords against a bloomfilter
+
+import bloom
+import sys
+import hashlib
+
+#bf = bloom.BloomFilter(array_size=(1024**3)*8,do_hashing=True)
+bf = bloom.BloomFilter(filename=sys.argv[1],array_size=(1024**2)*512,do_hashing=False,slice_bits=120,slices=7,ishex=True)
+
+print bf.check(hashlib.sha1(sys.argv[2]).hexdigest())
---
+++ loadbloom.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Author Dario Clavijo 2017
+# GPlv3
+
+import bloom
+import sys
+import fileinput
+
+SIZEMB=128
+bf = bloom.BloomFilter(array_size=(1024**2)*SIZEMB,do_hashing=False,slice_bits=120,slices=7,ishex=True)
+
+new=0
+seen=0
+fp = open(sys.argv[1],'r+')
+for line in fp:
+    try:
+        #h=str(int(line.rstrip(),16)).encode('utf8')
+        h = line.rstrip()
+        #print(h)
+    except:
+        h=None
+    if h != None:
+        if bf.update(h)==False:
+            new+=1
+        else:
+            seen+=1
+    print("new:%d seen:%d" %(new,seen))
+
+if new > 0:
+    bf.save(sys.argv[2])
+fp.close()
+
