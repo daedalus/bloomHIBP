be38269ff063694b901ad36abea993e0b473ed33
Darío Clavijo <clavijodario@gmail.com>
2020-11-21 01:44:07 -0300

    Update checkbloom.py

--- checkbloom.py
+++ checkbloom.py
@@ -11,4 +11,4 @@ import hashlib
 #bf = bloom.BloomFilter(array_size=(1024**3)*8,do_hashing=True)
 bf = bloom.BloomFilter(filename=sys.argv[1],array_size=(1024**2)*512,do_hashing=False,slice_bits=120,slices=7,ishex=True)
 
-print bf.check(hashlib.sha1(sys.argv[2]).hexdigest())
+print(bf.check(hashlib.sha1(sys.argv[2]).hexdigest()))
